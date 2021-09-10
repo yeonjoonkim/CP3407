fe88f9ef59151842cd162840ff6657603acd4836
Joon <yeonjoon.developer@gmail.com>
2021-09-10 13:32:18 +1000

    add new device component added.

--- src/app/device/device.page.ts
+++ src/app/device/device.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-device',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevicePage implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {
   }
